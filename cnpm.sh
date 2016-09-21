5971a3f5c2156af354b418507521328db9ed716f
leinue <597055914@qq.com>
2016-09-21 10:26:37 +0800

    first commit

---
+++ cnpm.sh
@@ -0,0 +1,6 @@
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+nvm install v4.2.2
+npm install -g cnpm --registry=https://registry.npm.taobao.org
